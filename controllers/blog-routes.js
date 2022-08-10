e5c46508c618c432cac5da84e8e3f1593ebe9d0e
justin <fezzygo@gmail.com>
2022-08-10 13:00:38 -0400

    updated the blog-routes(controllers/blog-routes.js:1-24)

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -1,7 +1,27 @@
+const { User, BlogPost } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', async (req, res)=>{
-    res.send("This is a test")
+    try {
+        // Get all posts
+        const postData = await BlogPost.findAll({
+            include: [{model: User}],
+        })
+        
+        // serialize post data
+        const posts = postData.map((project)=>project.get({plain:true}))
+
+        // Use serilized data
+        res.render("home", {posts})
+
+    } catch (error) {
+        res.render(500).json(error);
+    }
+})
+
+router.get('/login', async (req, res)=>{
+    res.render("login")
 })
 
 module.exports = router;
