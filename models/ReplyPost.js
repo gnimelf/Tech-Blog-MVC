859abf6bb1c85112c638e06a8223ff01fbaa5b9f
justin <fezzygo@gmail.com>
2022-08-10 17:30:03 -0400

    add ReplyPost model (models/ReplyPost.js)

---
+++ models/ReplyPost.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class ReplyPost extends Model {}
+
+ReplyPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        parent_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model:repl
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "blogPostReply",
+    }
+);
+
+module.exports = ReplyPost;
