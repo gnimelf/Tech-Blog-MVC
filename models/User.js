9df138c29136f1a8fac97b09fd3ed0b672a5f5e9
justin <fezzygo@gmail.com>
2022-08-13 22:18:50 -0400

    updated User.js model to include bcrypt

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
-const { Model, DataTypes } = require("sequelize")
-const sequelize = require("../config/connection")
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
@@ -22,14 +27,30 @@ User.init(
             validate: {
                 len: [8],
             },
-        }
+        },
     },
     {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(
+                    newUserData.password,
+                    10
+                );
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(
+                    updatedUserData.password,
+                    10
+                );
+                return updatedUserData;
+            },
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: "user",
     }
-)
-module.exports = User
+);
+module.exports = User;
