6f957722670a39e77ac7bccb1bdf5a0ba2872c9d
justin <fezzygo@gmail.com>
2022-08-13 23:02:42 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const PORT = process.env.port || 3001
 // Setting up handlebars
 app.engine('handlebars', hbs.engine) // Define engine
 app.set('view engine', 'handlebars'); // Set view engine to handle bars
-app.use(express.static(path.join(__dirname, 'public'))) // Set public folder
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Set the public folder for express
+app.use(express.static(path.join(__dirname, 'public'))) 
 
-app.use(routes) // Set routes
+//Use routes
+app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`))
