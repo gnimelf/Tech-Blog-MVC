94e43632490e97870159a1ce3d86807fa40f8bf3
justin <fezzygo@gmail.com>
2022-08-13 21:22:25 -0400

    renamed the login .js and signup.js files

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const signupHandler = async (event) => {
+    event.preventDefault();
+
+    const userName = document.querySelector("#username").value.trim();
+    const password = document.querySelector("#password").value.trim();
+
+    // Check username and password aren't empty
+    if (userName && password) {
+        console.log(userName + " " + password)
+        const response = await fetch("/api/users/", 
+            {
+                method: 'POST',
+                body: JSON.stringify({userName, password}),
+                headers: { 'Content-Type': 'Application/json'}
+            }
+        );
+
+        if (response.ok){
+            document.location.replace('/dashboard')
+        } else {
+            alert(response.statusText)
+        }
+    }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupHandler);
