77aeb99789d52cfba90e46ecdde0ff6138d6410a
justin <fezzygo@gmail.com>
2022-08-14 00:56:06 -0400

    removed responding with user creds in res (user-routes.js:54,55)

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -50,9 +50,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.loggedIn = true;
 
-      res
-        .status(200)
-        .json({ user: dbUserData, message: 'You are now logged in!' });
+      res.status(200).json({ message: 'You are now logged in!' });
     });
   } catch (err) {
     console.log(err);
